8f4b1e8be1c5f988cdb7d08df494a5fa7437b94d
JEphron <jephron63@gmail.com>
2019-06-15 20:12:33 -0700

    Prep for hash routing

--- src/Route.elm
+++ src/Route.elm
@@ -1,10 +1,39 @@
-module Route exposing (fromUrl, Route(..))
+module Route exposing (Route(..), fromUrl)
 
+import Html
+import Html.Attributes exposing (href)
 import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
 
-type Route =
-    Root
+
+type Route
+    = Root
+
+
+parser : Parser (Route -> a) a
+parser =
+    oneOf
+        [ Parser.map Root Parser.top
+        ]
+
 
 fromUrl : Url -> Maybe Route
 fromUrl url =
-    Just Root
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+        |> Parser.parse parser
+
+
+toParts : Route -> List String
+toParts route =
+    case route of
+        Root ->
+            []
+
+
+toHref : Route -> Html.Attribute msg
+toHref route =
+    let
+        parts =
+            toParts route
+    in
+    href ("#/" ++ String.join "/" parts)
